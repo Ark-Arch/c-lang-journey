11149869d716b428fa8c423606e97feb717f587f
David Ifeoluwa Agbemuko <davidagbemuko.ad@gmail.com>
2023-09-06 23:16:49 +0100

    add a simple program on variable usage, and its executable

---
+++ basic-sets/3variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	char characterName[] = "George";
+	int characterAge = 35;	
+
+	printf("There once was a man named %s\n", characterName);
+	printf("the was %d years old.\n", characterAge); 
+	printf("He really liked the name %s\n", characterName);
+	printf("but did not like being %d.\n", characterAge);
+	return 0;
+}
