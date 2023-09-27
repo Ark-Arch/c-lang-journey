15f4c804909a9a200f0b1592255f8152f0dc9d49
David Ifeoluwa Agbemuko <davidagbemuko.ad@gmail.com>
2023-09-27 16:03:54 +0100

    understanding the use of scanf and printf function prototypes of the stdio.h library

---
+++ al-pohl-book/4extra.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main (void)
+{
+	int  var1;
+	int  var2;
+
+	var1 = 77;
+	printf("\nprompt: type your variable on the next line.\n");
+	scanf("\n%d\n", &var2);
+	printf("\nvar1 = %d;\nvar2 = %d;\n", var1, var2);
+
+	return 0;
+}
+
+
+
